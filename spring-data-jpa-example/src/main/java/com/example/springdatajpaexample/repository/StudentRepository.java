e06136e468a7ddb7efefbe7cfbcdebd439eb3083
grapefriut422 <prikey7@gmail.com>
2024-07-03 15:25:07 +0900

    commit test

--- spring-data-jpa-example/src/main/java/com/example/springdatajpaexample/repository/StudentRepository.java
+++ spring-data-jpa-example/src/main/java/com/example/springdatajpaexample/repository/StudentRepository.java
@@ -4,6 +4,9 @@ import com.example.springdatajpaexample.domain.Students;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface StudentRepository extends JpaRepository<Students, Long> {
+    public List<Students> findAll();
 }
